df35fe4f7590efad2f76583ec8bdff0608c75793
unknown <mateusz.ozga@interia.pl>
2021-02-20 12:35:06 +0100

    1st, simpliest version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [elevation, setElevation] = useState(0);
+  const [lat, setLat] = useState(0);
+  const [long, setLong] = useState(0);
+  const apiKey = '';
+  //paste your google elevation API key into this string
+
+  const getElev = () => {
+    fetch('https://maps.googleapis.com/maps/api/elevation/json?locations='+lat+','+long+'&key='+apiKey)
+    .then(response => response.json())
+    .then(data => setElevation(Math.round(data.results[0].elevation)))
+    .catch(() => setElevation(0))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>This location's elevation is:</p>
+        <h1>{elevation===0 ? 'Please enter valid co-ordinates' : elevation+'m'}</h1>
+        <div>
+          <div>
+            <p>Latitude</p>
+            <input onChange={event => setLat(event.target.value)} />
+          </div>
+          <div>
+            <p>Longitude</p>
+            <input onChange={event => setLong(event.target.value)} />
+          </div>
+        </div>
+        <button onClick={() => getElev()}>Get elevation</button>
     </div>
   );
 }
